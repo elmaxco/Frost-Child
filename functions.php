fb297907606b8ea49c623eb8fbc850f54bb3a8de
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2025-12-15 12:58:13 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Frost Child Theme Functions
+ */
+
+if ( ! function_exists( 'frost_child_setup' ) ) {
+	function frost_child_setup() {
+		// Enqueue parent theme stylesheet
+		wp_enqueue_style( 'frost-parent-style', get_template_directory_uri() . '/style.css' );
+		
+		// Enqueue child theme stylesheet
+		wp_enqueue_style( 'frost-child-style', get_stylesheet_uri() );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'frost_child_setup' );
